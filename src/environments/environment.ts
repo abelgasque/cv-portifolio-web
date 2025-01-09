df6768297e0b4c22370ea52aa3915d362b7dd8ac
Abel Gasque <abelgasque20@gmail.com>
2025-01-09 15:06:24 -0300

    feat: Update file env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: false,
-  version: "v1.0.0",
+  version: "v1.0.1",
+  firebaseConfig: {
+    apiKey: "Your Firebase API Key",
+    authDomain: "Your Firebase Auth Domain",
+    projectId: "Your Firebase Project ID",
+    storageBucket: "Your Firebase Storage Bucket",
+    messagingSenderId: "Your Firebase Messaging Sender ID",
+    appId: "Your Firebase App ID",
+    measurementId: "Your Firebase Measurement ID"
+  },
 };
