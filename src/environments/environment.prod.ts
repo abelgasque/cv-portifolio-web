bf93afd6ef0d08f299cbb976e4d21893bc32996f
Abel Gasque <abelgasque20@gmail.com>
2025-01-14 21:02:01 -0300

    Versao 1.0.10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
-  version: "v1.0.9",
+  version: "v1.0.10",
   avatarImg: "{{FIREBASE_AVATAR_IMG}}",
   cvImg: "{{FIREBASE_CV_IMG}}",
   firebaseConfig: {
     apiKey: "{{FIREBASE_API_KEY}}",
     authDomain: "{{FIREBASE_AUTH_DOMAIN}}",
+    databaseURL: "{{FIREBASE_DB_URL}}",
     projectId: "{{FIREBASE_PROJECT_ID}}",
     storageBucket: "{{FIREBASE_STORAGE_BUCKET}}",
     messagingSenderId: "{{FIREBASE_MESSAGING_SENDER_ID}}",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
-  version: "v1.0.9",
+  version: "v1.0.10",
   avatarImg: "assets/images/avatar.jpg",
   cvImg: "assets/images/cv.pdf",
   firebaseConfig: {
     apiKey: "Your Firebase API Key",
     authDomain: "Your Firebase Auth Domain",
+    databaseURL: "Your database URL",
     projectId: "Your Firebase Project ID",
     storageBucket: "Your Firebase Storage Bucket",
     messagingSenderId: "Your Firebase Messaging Sender ID",
