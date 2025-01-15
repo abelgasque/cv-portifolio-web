01813db8239bb6a6f22eac8614dc07e29bb6b9d3
Abel Gasque <abelgasque20@gmail.com>
2025-01-15 12:39:36 -0300

    Versao 1.0.10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: "v1.0.10",
+  version: "v1.0.11",
   avatarImg: "assets/images/avatar.jpg",
   cvImg: "assets/images/cv.pdf",
   firebaseConfig: {
