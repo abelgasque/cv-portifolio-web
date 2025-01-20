a3360b36633a104cedb47090d4192f237a3cde45
Abel Gasque <abelgasque20@gmail.com>
2025-01-20 17:04:41 -0300

    Ajustes ci cd

--- server.js
+++ server.js
@@ -11,28 +11,38 @@ const {
   FIREBASE_CONFIG
 } = process.env;
 
-const production = (APP_DEBUG === "production") ? true : false;
+const production = (APP_DEBUG === "production");
 const dir = "src/environments";
-const prodFile = (production) ? "environment.prod.ts" : "environment.ts";
+const prodFile = production ? "environment.prod.ts" : "environment.ts";
+
+let appConfig, firebaseConfig;
+
+try {
+  appConfig = JSON.parse(APP_CONFIG);
+  firebaseConfig = JSON.parse(FIREBASE_CONFIG);
+} catch (e) {
+  console.error("Erro ao parsear variáveis de ambiente JSON:", e);
+  process.exit(1);
+}
 
 const formattedContent = `export const environment = {
   production: ${production},
   name: "${APP_NAME}",
   version: "${APP_VERSION}",
-  appConfig: ${APP_CONFIG},
-  firebaseConfig: ${FIREBASE_CONFIG},
+  appConfig: ${JSON.stringify(appConfig, null, 2)},
+  firebaseConfig: ${JSON.stringify(firebaseConfig, null, 2)},
 };`;
 
 fs.access(dir, fs.constants.F_OK, (err) => {
   if (err) {
-    console.log("src doesn't exist, creating now", process.cwd());
     fs.mkdirSync(dir, { recursive: true });
   }
+  
   try {
     fs.writeFileSync(path.join(dir, prodFile), formattedContent);
-    console.log(`Successfully created ${prodFile}`);
+    console.log(`Arquivo ${prodFile} criado com sucesso!`);
   } catch (error) {
-    console.error(error);
+    console.error("Erro ao escrever o arquivo:", error);
     process.exit(1);
   }
 });
