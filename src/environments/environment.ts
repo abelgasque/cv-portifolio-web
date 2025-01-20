7c741e5b4b2e1e807a0a9c2beb8cb111d7631d38
Abel Gasque <abelgasque20@gmail.com>
2025-01-20 13:11:35 -0300

    revert version app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  version: "v1.0.16",
-  avatarImg: "https://firebasestorage.googleapis.com/v0/b/cv-abelgasque-e8ab0.firebasestorage.app/o/assets%2Favatar-1.jpg?alt=media&token=ee7a745e-22fe-40e9-8e12-2d9cd071c682",
-  cvImg: "https://firebasestorage.googleapis.com/v0/b/cv-abelgasque-e8ab0.firebasestorage.app/o/assets%2Fcv.pdf?alt=media&token=848e3e2a-bb9e-4a32-8e46-abce1b9c3f87",
+  version: "v1.0.15",
+  avatarImg: "./assets/images/avatar.jpg",
+  cvImg: "./assets/images/cv.pdf",
   firebaseConfig: {
     apiKey: "Your Firebase API Key",
     authDomain: "Your Firebase Auth Domain",
