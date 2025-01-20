2bc17258d2d61d587f79b545e9da145532b046c3
Abel Gasque <abelgasque20@gmail.com>
2025-01-20 17:09:38 -0300

    ajustes prebuild

--- server.js
+++ server.js
@@ -15,22 +15,12 @@ const production = (APP_DEBUG === "production");
 const dir = "src/environments";
 const prodFile = production ? "environment.prod.ts" : "environment.ts";
 
-let appConfig, firebaseConfig;
-
-try {
-  appConfig = JSON.parse(APP_CONFIG);
-  firebaseConfig = JSON.parse(FIREBASE_CONFIG);
-} catch (e) {
-  console.error("Erro ao parsear variáveis de ambiente JSON:", e);
-  process.exit(1);
-}
-
 const formattedContent = `export const environment = {
   production: ${production},
   name: "${APP_NAME}",
   version: "${APP_VERSION}",
-  appConfig: ${JSON.stringify(appConfig, null, 2)},
-  firebaseConfig: ${JSON.stringify(firebaseConfig, null, 2)},
+  appConfig: ${APP_CONFIG},
+  firebaseConfig: ${FIREBASE_CONFIG},
 };`;
 
 fs.access(dir, fs.constants.F_OK, (err) => {
